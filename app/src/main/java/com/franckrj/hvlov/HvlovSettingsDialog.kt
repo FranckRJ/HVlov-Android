04782db556ad27252f0c07ec3699243914ca56ac
FranckRJ <franck.rj.w@gmail.com>
2020-02-26 12:55:47 +0100

    Mise en place du ViewBinding pour HvlovSettingsDialog.

--- app/src/main/java/com/franckrj/hvlov/HvlovSettingsDialog.kt
+++ app/src/main/java/com/franckrj/hvlov/HvlovSettingsDialog.kt
@@ -2,9 +2,9 @@ package com.franckrj.hvlov
 
 import android.app.Dialog
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import com.franckrj.hvlov.databinding.DialogHvlovsettingsBinding
 import com.google.android.material.textfield.TextInputLayout
 
 class HvlovSettingsDialog : DialogFragment() {
@@ -13,10 +13,11 @@ class HvlovSettingsDialog : DialogFragment() {
         const val ARG_SERVER_PASSWORD = "com.franckrj.hvlov.HvlovSettingsDialog.ARG_SERVER_PASSWORD"
     }
 
+    private lateinit var _binding: DialogHvlovsettingsBinding
+
     var onDialogResult: ((serverAdress: String, serverPassword: String) -> Unit)? = null
 
-    fun unanimatlySetTextInputLayoutText(textInputLayout: TextInputLayout, text: String)
-    {
+    fun unanimatlySetTextInputLayoutText(textInputLayout: TextInputLayout, text: String) {
         val oldAnimEnabledValue: Boolean = textInputLayout.isHintAnimationEnabled
         textInputLayout.isHintAnimationEnabled = false
         textInputLayout.editText?.setText(text)
@@ -24,25 +25,22 @@ class HvlovSettingsDialog : DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val dialogLayout: View = requireActivity().layoutInflater.inflate(R.layout.dialog_hvlovsettings, null)
+        _binding = DialogHvlovsettingsBinding.inflate(requireActivity().layoutInflater, null, false)
 
         val serverAdress: String = arguments?.getString(ARG_SERVER_ADRESS, null) ?: ""
         val serverPassword: String = arguments?.getString(ARG_SERVER_PASSWORD, null) ?: ""
 
-        val serverAdressLayout: TextInputLayout = dialogLayout.findViewById(R.id.layout_serveradress_hvlovsettings)
-        val serverPasswordLayout: TextInputLayout = dialogLayout.findViewById(R.id.layout_serverpassword_hvlovsettings)
-
-        unanimatlySetTextInputLayoutText(serverAdressLayout, serverAdress)
-        unanimatlySetTextInputLayoutText(serverPasswordLayout, serverPassword)
+        unanimatlySetTextInputLayoutText(_binding.layoutServeradressHvlovsettings, serverAdress)
+        unanimatlySetTextInputLayoutText(_binding.layoutServerpasswordHvlovsettings, serverPassword)
 
         val builder = AlertDialog.Builder(requireActivity()).apply {
             setTitle(R.string.settings)
-            setView(dialogLayout)
+            setView(_binding.root)
             setNegativeButton(R.string.cancel) { dialog, _ -> dialog.dismiss() }
             setPositiveButton(R.string.apply) { dialog, _ ->
                 onDialogResult?.invoke(
-                    serverAdressLayout.editText?.text?.toString() ?: "",
-                    serverPasswordLayout.editText?.text?.toString() ?: ""
+                    _binding.layoutServeradressHvlovsettings.editText?.text?.toString() ?: "",
+                    _binding.layoutServerpasswordHvlovsettings.editText?.text?.toString() ?: ""
                 )
                 dialog.dismiss()
             }
