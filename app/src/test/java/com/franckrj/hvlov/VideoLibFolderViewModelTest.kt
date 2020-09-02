456ccd52c4ba04b91738c9bbc056a98521732e38
FranckRJ <franck.rj.w@gmail.com>
2020-09-02 16:24:18 +0200

    Correction du test d'integration.

--- app/src/test/java/com/franckrj/hvlov/VideoLibFolderViewModelTest.kt
+++ app/src/test/java/com/franckrj/hvlov/VideoLibFolderViewModelTest.kt
@@ -6,34 +6,22 @@ import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.mockk
-import io.mockk.slot
+import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.After
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
 class VideoLibFolderViewModelTest {
-    companion object {
-        private const val SAVE_CURRENT_PATH: String = "SAVE_CURRENT_PATH"
-    }
-
-    val hvlovServerSettings = HvlovServerSettings("some_url", "some_password", 1)
-    var currentPath = ""
-    val currentPathSlot = slot<String>()
-
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
-
-    val mockHvlovPreferencesService = mockk<HvlovPreferencesService>()
 
     val mockHvlovRepository = mockk<HvlovRepository>()
 
@@ -44,11 +32,7 @@ class VideoLibFolderViewModelTest {
 
     @Before
     fun setup() {
-        every { mockHvlovPreferencesService.hvlovServerSettings } returns MutableStateFlow(hvlovServerSettings)
-        every { mockSavedStateHandle.get<String>(SAVE_CURRENT_PATH) } returns currentPath
-        every { mockSavedStateHandle.set(SAVE_CURRENT_PATH, capture(currentPathSlot)) } answers {
-            currentPath = currentPathSlot.captured
-        }
+        every { mockSavedStateHandle.get<String>("ARG_FOLDER_PATH") } returns ""
         every { mockHvlovRepository.getEntriesForPath(any()) } returns flow { }
 
         Dispatchers.setMain(TestCoroutineDispatcher())
@@ -61,9 +45,7 @@ class VideoLibFolderViewModelTest {
     }
 
     @Test
-    fun `goToPreviousFolder -- do nothing when there isn't a previous folder`() {
-        currentPath = ""
-        assertEquals(false, videoLibFolderViewModel.goToPreviousFolder())
-        assertEquals("", currentPath)
+    fun `construction -- try to get entries from the repository`() {
+        verify { mockHvlovRepository.getEntriesForPath("") }
     }
 }
